35cc1c7c80e43478715c9c50b07fa12f65ad8ea9
Michal Josef Špaček <tupinek@gmail.com>
2018-02-07 08:08:59 +0100

    Prepare for 0.04 version

--- Search.pm
+++ Search.pm
@@ -7,7 +7,7 @@ use CPAN;
 use Class::Utils qw(set_params);
 use Getopt::Std;
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -139,6 +139,6 @@ L<http://skim.cz>
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut

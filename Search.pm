67fb5005aae5976b76f7e6be2c6c721df05d5259
Michal Josef Špaček <tupinek@gmail.com>
2022-01-05 12:18:40 +0100

    Prepare for 0.07 version

--- Search.pm
+++ Search.pm
@@ -6,7 +6,7 @@ use warnings;
 use CPAN;
 use Getopt::Std;
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Constructor.
 sub new {
@@ -133,6 +133,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
